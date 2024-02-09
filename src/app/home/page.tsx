b78c708a8a344bbf95d59a1e5a750c57190a90d8
Luke <luke.brannagan@kroo.com>
2024-02-09 22:27:20 +0000

    feat: add redirect for home for storyblok preview

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,36 +1,5 @@
-'use client';
-
-import { gql, useQuery } from '@apollo/client';
-import {
-  ISbStoryData,
-  StoryblokComponent,
-  useStoryblokState,
-} from '@storyblok/react';
-import { PageStoryblok } from '../../types/component-types-sb';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const { data } = useQuery<{ PageItem: ISbStoryData<PageStoryblok> }>(query);
-
-  const story = useStoryblokState(data?.PageItem || null);
-
-  console.log(story);
-
-  if (!story?.content) {
-    return <div>Loading...</div>;
-  }
-
-  return <StoryblokComponent blok={story.content} />;
+  redirect('/');
 }
-
-const query = gql`
-  {
-    PageItem(id: "home") {
-      content {
-        components
-        component
-        _uid
-        _editable
-      }
-    }
-  }
-`;
