c867ffa39e3e739526e743260c0855872aafb59a
Luke <luke.brannagan@kroo.com>
2024-02-25 18:54:59 +0000

    feat: initial meta tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,13 @@ import {
   useStoryblokState,
 } from '@storyblok/react';
 import { client } from '@components/Providers/ApolloClient';
+import Head from 'next/head';
 import { PageStoryblok } from '../types/component-types-sb';
 
 export type THomePageResponse = {
   PageItem: ISbStoryData<PageStoryblok>;
 };
+
 export type TIndexPageProps = {
   pageProps: {
     storyblok: THomePageResponse;
@@ -25,6 +27,27 @@ export default function Home({ pageProps: { storyblok } }: TIndexPageProps) {
 
   return (
     <div>
+      <Head>
+        <title>Luke Brannagan | Software Developer</title>
+        <meta name="title" content="Luke Brannagan | Software Developer" />
+        <meta
+          name="description"
+          content="Website and blog that offers advice on indie hacking and software development"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://lukebrannagan.com/" />
+        <meta
+          property="og:title"
+          content="Luke Brannagan | Software Developer"
+        />
+        <meta
+          property="og:description"
+          content="Website and blog that offers advice on indie hacking and software development"
+        />
+        <meta property="og:image" content="/META.png" />
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <StoryblokComponent blok={story.content} />
     </div>
   );
