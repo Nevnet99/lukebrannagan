3c17f156d24cf14991811a0ae2dea2b261efd637
Luke <luke.brannagan@kroo.com>
2024-03-13 18:38:32 +0000

    feat: implemented smooth scrolling

--- src/components/shared/Layout/index.tsx
+++ src/components/shared/Layout/index.tsx
@@ -1,9 +1,10 @@
 import { Footer } from '@components/Footer';
 import { Navigation } from '@components/Navigation';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 // eslint-disable-next-line camelcase
 import { Manrope, Fjalla_One } from 'next/font/google';
 import clsx from 'clsx';
+import Lenis from '@studio-freight/lenis';
 
 const manrope = Manrope({ subsets: ['latin'], variable: '--font-manrope' });
 const fjallaOne = Fjalla_One({
@@ -12,10 +13,25 @@ const fjallaOne = Fjalla_One({
   variable: '--font-fjalla',
 });
 
-export const Layout = ({ children }: { children: ReactNode }) => (
-  <div className={clsx(manrope.variable, fjallaOne.variable)}>
-    <Navigation />
-    <main>{children}</main>
-    <Footer />
-  </div>
-);
+export const Layout = ({ children }: { children: ReactNode }) => {
+  useEffect(() => {
+    // ! Adds smoothscroll to the project
+    if (typeof window === 'undefined') return;
+    const lenis = new Lenis();
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
+  return (
+    <div className={clsx(manrope.variable, fjallaOne.variable)}>
+      <Navigation />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
