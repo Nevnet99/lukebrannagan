df29b2c993b61c08b7cfb26a4f3415169153bcab
Luke <luke.brannagan@kroo.com>
2024-03-14 19:03:07 +0000

    fix: on mount fix

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -7,6 +7,9 @@ export const useBreakpoint = () => {
     const handleResize = () => {
       setBreakpoint(window.innerWidth);
     };
+    if (typeof window !== 'undefined') {
+      setBreakpoint(window.innerWidth);
+    }
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
