b1a42f5d2ec23f06298ddde67debdae88fa7ab5c
Luke <luke.brannagan@kroo.com>
2024-06-27 14:42:24 +0100

    feat: remove cursor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,12 @@ import { Providers } from '@components/Providers';
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { Layout } from '@components/shared/Layout';
-import { Cursor } from '@components/Cursor';
-import { useBreakpoint } from '../hooks/useWindowSize';
 
 export default function App({ Component, ...rest }: AppProps) {
-  const breakpoint = useBreakpoint();
-  const NOTTabletORMobile = breakpoint >= 1024;
-
   return (
     <Providers>
       <Layout>
         <Component {...rest} />
-        {NOTTabletORMobile && <Cursor />}
       </Layout>
     </Providers>
   );
